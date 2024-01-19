05f6b70a2681cdfcf32a4b905b0a36ee5bbad6d6
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-01-19 09:08:40 +0530

    create nested element

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" }, // attribute to element
-  "Playing with React"
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>I am a h1 heading</h1>
+  </div>
+</div>; */
+}
+
+// creating an object
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am a h1 heading")
+  )
 );
 
-console.log(heading); // object
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
