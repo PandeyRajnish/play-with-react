f55d8bff242e2f0606a52eab3c5ed09e799d37a0
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-01-19 09:21:29 +0530

    create nested structure

--- App.js
+++ App.js
@@ -4,18 +4,26 @@
     <h1>I am a h1 heading</h1>
     <h2>I am a h2 heading</h2>
   </div>
+  <div id="child2">
+    <h1>I am a h1 heading</h1>
+    <h2>I am a h2 heading</h2>
+  </div>
 </div>; */
 }
 
 // creating an object
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am a h1 heading"),
     React.createElement("h2", {}, "I am a h2 heading"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 heading"),
+    React.createElement("h2", {}, "I am a h2 heading"),
+  ]),
+]);
+
+//JSX- because of this messy nesting structure JSX came into the picture.
 
 console.log(parent); // object
 
