41219004a79b33717eb2453f9bdec666aaa9158b
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-01-21 02:17:48 +0530

    ignites our app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a h1 heading"),
+    React.createElement("h1", {}, "Playing with react 🚀"),
     React.createElement("h2", {}, "I am a h2 heading"),
   ]),
   React.createElement("div", { id: "child2" }, [
